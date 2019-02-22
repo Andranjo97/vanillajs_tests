0fba5eeb92268c5df84855e24abb658c551ba527
jnaranjo <jnaranjo@ioet.com>
2019-02-22 18:19:58 -0500

    add coment

--- ToDo.js
+++ ToDo.js
@@ -1,55 +1,56 @@
-class ToDo{
-    constructor(){
-        this.todo =[];
-    }
+class ToDo {
+  //add comment
+  constructor() {
+    this.todo = [];
+  }
 
-    addTodo(item) {
-        this.todo.push(item);
-    }
+  addTodo(item) {
+    this.todo.push(item);
+  }
 
-    getItems() {
-        return this.todo;
-    }
+  getItems() {
+    return this.todo;
+  }
 
-    delete(id) {
-        this.todo = this.todo.filter(item => item.id !== id);
-    }
+  delete(id) {
+    this.todo = this.todo.filter(item => item.id !== id);
+  }
 
-    complete(id) {
-        this.todo.find(item => item.id === id).complete = true;
-    }
+  complete(id) {
+    this.todo.find(item => item.id === id).complete = true;
+  }
 }
 
 class DomManipulation {
-    init(){
-        const form = document.createElement('form');
-        const input = document.createElement('input');
-        const ul = document.createElement('ul');
-        input.id = "AddItemInput";
-        form.id = "addItemForm";
-        form.appendChild(input);
+  init() {
+    const form = document.createElement("form");
+    const input = document.createElement("input");
+    const ul = document.createElement("ul");
+    input.id = "AddItemInput";
+    form.id = "addItemForm";
+    form.appendChild(input);
 
-        return{
-            form,
-            ul
-        }
-    }
+    return {
+      form,
+      ul
+    };
+  }
 
-    displayItem(item) {
-        const li = document.createElement('li');
-        li.innerText = item.title;
-        return li;
-    }
+  displayItem(item) {
+    const li = document.createElement("li");
+    li.innerText = item.title;
+    return li;
+  }
 
-    addTodoEvent(form, createTodo, unorderedList){
-        const displayItem = this.displayItem;
-        const id = new Date().getUTCMilliseconds();
-        form.addEventListener('submit', function(e){
-            e.preventDefault();
-            const input = document.querySelector('input').value;
-            const item = {complete: false,id : id, title: input};
-            createTodo(item);
-            unorderedList.appendChild(displayItem(item))
-        })
-    }
+  addTodoEvent(form, createTodo, unorderedList) {
+    const displayItem = this.displayItem;
+    const id = new Date().getUTCMilliseconds();
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      const input = document.querySelector("input").value;
+      const item = { complete: false, id: id, title: input };
+      createTodo(item);
+      unorderedList.appendChild(displayItem(item));
+    });
+  }
 }
